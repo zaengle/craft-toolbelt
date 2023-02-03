d62e1ba2f5d2dc0cfcb0c43c17bfd8e1d1df4db9
Tom Davies <tom@tomdavies.net>
2023-02-03 11:19:59 +0000

    added: settings to support custom helpers

--- src/Models/Settings.php
+++ src/Models/Settings.php
@@ -8,10 +8,10 @@ class Settings extends Model
 {
     public array $svgPaths = [];
     public string $svgSpriteIdPrefix = 'sprite-';
-
     public string $svgSpriteTemplate = '_toolbelt/useSvgSprite';
     public array $svgSpriteDefaultOpts = [];
     public array $svgSpriteDefaultAttrs = [];
+    public array $custom = [];
 
     public function rules(): array
     {
